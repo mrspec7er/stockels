a4b99e6ba0f076bb78dd2c7ba0d2c976b363cc18
mrspec7er <wijayakusumasandi@gmail.com>
2023-09-25 12:31:23 +0800

    feat: 🎸 (user): added service for register and login

---
+++ module/user/user.service.go
@@ -0,0 +1,46 @@
+package user
+
+import (
+	"os"
+	"stockels/models"
+	"stockels/utils"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CreateUserService(user models.User) (models.User, error) {
+	encryptedPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), 11);
+	if err != nil {
+		return models.User{}, err
+	}
+
+	userEntries := models.User{FullName: user.FullName,Email: user.Email,Password: string(encryptedPass),IsVerified: false}
+	err = utils.DB().Create(&userEntries).Error
+
+	return userEntries, err
+}
+
+func LoginService(email string, password string) (string, error) {
+	user := models.User{}
+	err := utils.DB().First(&user, "email = ?", email).Error
+	if err != nil {
+		return  "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password));
+	if err != nil {
+		return  "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id": user.ID,
+		"exp": time.Now().Add(time.Duration(time.Now().Day())).Unix(),
+	})
+
+	exportToken, err := token.SignedString([]byte(os.Getenv("TOKEN_SECRET")))
+
+	return exportToken, err
+
+}
