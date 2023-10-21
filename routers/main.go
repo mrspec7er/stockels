6ad5648d91aa099527c33321529ed94b4abc5340
mrspec7er <wijayakusumasandi@gmail.com>
2023-10-21 17:38:08 +0800

    feat: 🎸 (*): GraphQL Init

--- routers/main.go
+++ routers/main.go
@@ -1,15 +1,41 @@
 package routers
 
 import (
+	"stockels/graph"
+	"stockels/middleware"
 	"stockels/module/stock"
 	"stockels/module/user"
 
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gin-gonic/gin"
 )
+
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
 func Config()  {
 	router := gin.Default()
 	stock.Routes(router)
 	user.Routes(router)
+	router.Use(middleware.GinContextToContextMiddleware())
+	router.POST("/query", graphqlHandler())
+	router.GET("/", playgroundHandler())
 	router.Run()
 }
