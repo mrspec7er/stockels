391cbd1d1701560b5db7339645d5f58fe6724d59
mrspec7er <wijayakusumasandi@gmail.com>
2023-10-25 20:08:07 +0800

    refactor: 💡 (rest): separate stock and subscribtion service

--- rest/stock/stock.route.go
+++ rest/stock/stock.route.go
@@ -11,9 +11,9 @@ import (
 		{
 			routerGroup.POST("/stocks", GetStocks)
 			routerGroup.GET("/stocks/:symbol", GetStockDetail)
-			routerGroup.POST("/stocks/subscribe", middleware.Authentication, SubscribeToStocks)
-			routerGroup.GET("/stocks/subscribe", middleware.Authentication, GetSubscribtionStocks)
+			// routerGroup.POST("/stocks/subscribe", middleware.Authentication, SubscribeToStocks)
+			// routerGroup.GET("/stocks/subscribe", middleware.Authentication, GetSubscribtionStocks)
 			routerGroup.POST("/stocks/report", middleware.Authentication, GetStocksReport)
-			routerGroup.POST("/stocks/generate-report", middleware.Authentication, GenerateStockReport)
+			// routerGroup.POST("/stocks/generate-report", middleware.Authentication, GenerateStockReport)
 		}
 	}
