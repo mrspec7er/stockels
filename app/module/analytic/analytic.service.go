62a070b553897931cc654d14e207c3d9f931c6a3
mrspec7er <wijayakusumasandi@gmail.com>
2023-11-04 11:33:55 +0800

    feat: 🎸 (analytic): added logioc to stored data to redis

--- app/module/analytic/analytic.service.go
+++ app/module/analytic/analytic.service.go
@@ -1,33 +1,58 @@
 package analytic
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"stockels/app/object"
+	"stockels/app/utils"
+	"time"
 )
 
 func GetAnalyticFromAPI(symbol string) (*object.Analytic, error){
-	fmt.Println("Fetching business news to serpapi.com")
-	upstreamSerpApiKey := os.Getenv("UPSTREAM_SERP_API_KEY")
 	analyticData := &object.Analytic{}
 
-	res, err := http.Get("https://serpapi.com/search.json?engine=google_finance&q="+ symbol+ ":IDX&api_key="+ upstreamSerpApiKey)
-	if err != nil {
+	cachedKey := symbol + "_Analytic"
+	ctx := context.Background()
+	cachedStockAnalytic, err := utils.Cache().Get(ctx, cachedKey).Result()
+	if err != nil {	
+		fmt.Println("Fetching business news to serpapi.com")
+		upstreamSerpApiKey := os.Getenv("UPSTREAM_SERP_API_KEY")
+	
+		res, err := http.Get("https://serpapi.com/search.json?engine=google_finance&q="+ symbol+ ":IDX&api_key="+ upstreamSerpApiKey)
+		if err != nil {
+			return analyticData, err
+		}
+	
+		analyticStreamMetaData, err := io.ReadAll(res.Body)
+		if err != nil {
+			return analyticData, err
+		}
+		
+		err = json.Unmarshal(analyticStreamMetaData, &analyticData)
+		if err != nil {
+			return analyticData, err
+		}
+		err = CacheStockAnalytic(cachedKey, analyticData)
 		return analyticData, err
 	}
 
-	analyticStreamMetaData, err := io.ReadAll(res.Body)
-	if err != nil {
-		return analyticData, err
-	}
-
-	err = json.Unmarshal(analyticStreamMetaData, &analyticData)
-	if err != nil {
-		return analyticData, err
-	}
+	err = json.Unmarshal([]byte(cachedStockAnalytic), &analyticData)
 	
 	return analyticData, err
+}
+
+func CacheStockAnalytic(key string, stockAnalytic *object.Analytic) error {
+	ctx := context.Background()
+	stockAnalyticStringified, err := json.Marshal(stockAnalytic)
+
+	if err != nil {
+		return err
+	}
+	err = utils.Cache().Set(ctx, key, stockAnalyticStringified, time.Hour).Err()
+
+	return err
 }
