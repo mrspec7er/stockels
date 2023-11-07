c02bc7bd50a66353e0493097013447d444d3e8b6
mrspec7er <wijayakusumasandi@gmail.com>
2023-11-07 22:22:32 +0800

    test: 💍 (stock): added desc to test

--- app/services/stock/stock_test.go
+++ app/services/stock/stock_test.go
@@ -12,36 +12,69 @@ func init()  {
 	testings.SetupRouters("../../../.env")
 }
 
-func TestGetStockFromAPI(t *testing.T)  {
-	stocks := &object.StockData{}
+func TestStockService(t *testing.T)  {
+	t.Run("should return stock info", func(t *testing.T) {
+		stocks := &object.StockData{}
 
-	result, err := stock.GetStockInfoFromAPI("BBCA")
+		result, err := stock.GetStockInfoFromAPI("BBCA")
+	
+		if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stocks)) {
+			t.Errorf(err.Error())
+		}
+	})
 
-	if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stocks)) {
-		t.Errorf(err.Error())
-	}
+	t.Run("should return stock price", func(t *testing.T) {
+		stockDetail := []*object.StockDetailPrice{}
+
+		result, err := stock.GetStockPriceFromAPI("BBNI", "2023-05-11", "2023-11-2")
+
+		if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stockDetail)) {
+			t.Errorf(err.Error())
+		}
+	})
+
+	t.Run("should return stoock info", func(t *testing.T) {
+		getStocksPayload := []*object.GetStockData{{StockSymbol: "INDF", SupportPrice: 6275, ResistancePrice: 7550}, {StockSymbol: "ASII", SupportPrice: 5380, ResistancePrice: 6750}}
+
+		stocks := []*object.StockData{}
+
+		result, err := stock.GetMultipleStockService(getStocksPayload)
+
+		if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stocks)) {
+			t.Errorf(err.Error())
+		}
+	})
 }
 
-func TestGetStockDetailFromAPI(t *testing.T)  {
-	stockDetail := []*object.StockDetailPrice{}
+// func TestGetStockFromAPI(t *testing.T)  {
+// 	stocks := &object.StockData{}
 
-	result, err := stock.GetStockPriceFromAPI("BBNI", "2023-05-11", "2023-11-2")
+// 	result, err := stock.GetStockInfoFromAPI("BBCA")
 
-	if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stockDetail)) {
-		t.Errorf(err.Error())
-	}
+// 	if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stocks)) {
+// 		t.Errorf(err.Error())
+// 	}
+// }
 
-}
+// func TestGetStockDetailFromAPI(t *testing.T)  {
+// 	stockDetail := []*object.StockDetailPrice{}
 
-func TestGetMultipleStock(t *testing.T)  {
-	getStocksPayload := []*object.GetStockData{{StockSymbol: "INDF", SupportPrice: 6275, ResistancePrice: 7550}, {StockSymbol: "ASII", SupportPrice: 5380, ResistancePrice: 6750}}
+// 	result, err := stock.GetStockPriceFromAPI("BBNI", "2023-05-11", "2023-11-2")
 
-	stocks := []*object.StockData{}
+// 	if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stockDetail)) {
+// 		t.Errorf(err.Error())
+// 	}
 
-	result, err := stock.GetMultipleStockService(getStocksPayload)
+// }
 
-	if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stocks)) {
-		t.Errorf(err.Error())
-	}
+// func TestGetMultipleStock(t *testing.T)  {
+// 	getStocksPayload := []*object.GetStockData{{StockSymbol: "INDF", SupportPrice: 6275, ResistancePrice: 7550}, {StockSymbol: "ASII", SupportPrice: 5380, ResistancePrice: 6750}}
 
-}
+// 	stocks := []*object.StockData{}
+
+// 	result, err := stock.GetMultipleStockService(getStocksPayload)
+
+// 	if err != nil || (reflect.TypeOf(result) != reflect.TypeOf(stocks)) {
+// 		t.Errorf(err.Error())
+// 	}
+// }
--- testings/stock_test.go
+++ testings/stock_test.go
@@ -14,30 +14,34 @@ import (
 )
 
 
-func TestGetStockAnalytic(t *testing.T)  {
+func TestStockController(t *testing.T)  {
 	router := SetupRouters("../.env")
 	w := httptest.NewRecorder()
 
-	body := []object.GetStockData{{StockSymbol: "ICBP", SupportPrice: 9300, ResistancePrice: 11500}}
+	t.Run("should generate report files", func(t *testing.T) {
 
-	bodyJSON, err := json.Marshal(body)
-	payload := bytes.NewBuffer(bodyJSON)
-	if err != nil {
-		   fmt.Printf("server: could not read request body: %s\n", err)
-	}
+		body := []object.GetStockData{{StockSymbol: "ICBP", SupportPrice: 9300, ResistancePrice: 11500}}
+	
+		bodyJSON, err := json.Marshal(body)
+		payload := bytes.NewBuffer(bodyJSON)
+		if err != nil {
+			   fmt.Printf("server: could not read request body: %s\n", err)
+		}
+	
+		req, err := http.NewRequest("POST", "/api/v1/stocks/generate-report", payload)
+		req.Header.Set("Content-Type", "application/json")
+	
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	
+		router.ServeHTTP(w, req)
+	
+		assert.Equal(t, 200, w.Code)
+	
+		if (reflect.TypeOf(w.Body) != reflect.TypeOf(bytes.NewBufferString(""))) {
+			t.Errorf(err.Error())
+		}
+	})
 
-	req, err := http.NewRequest("POST", "/api/v1/stocks/generate-report", payload)
-	req.Header.Set("Content-Type", "application/json")
-
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-
-	if (reflect.TypeOf(w.Body) != reflect.TypeOf(bytes.NewBufferString(""))) {
-		t.Errorf(err.Error())
-	}
 }
